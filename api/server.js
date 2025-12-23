6a074e5e28b38cb3703496099ee4276445927f48
RazorManRoblox <achow6430@gmail.com>
2025-12-22 22:45:57 -0500

    Refactor static file serving in server.js
    Updated static file serving to improve clarity.

--- api/server.js
+++ api/server.js
@@ -7,18 +7,13 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// serve frontend
-app.use("/public", express.static(path.join(__dirname, "../public")));
+// serve static files
+app.use(express.static(path.join(__dirname, "../public")));
 app.use("/uv", express.static(path.join(__dirname, "../uv")));
 
 // homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-// fallback
-app.get("*", (req, res) => {
-  res.redirect("/");
 });
 
 export default app;
